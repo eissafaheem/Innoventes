0c4019b3abb28f29996a6d11b1164fe9372c1c70
Eissa Faheen <eissafaheem@gmail.com>
2025-03-25 14:48:46 +0530

    Initial react native

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+
+
+
+function App(): React.JSX.Element {
+  
+  return (
+    <View>
+      <Text>Eissa</Text>
+    </View>
+  );
+}
+
+
+export default App;
